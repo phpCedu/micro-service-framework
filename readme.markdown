9ce8444187c8fada443db216e602f73ad8351a5a
Alan W Szlosek Jr <alan.szlosek@gmail.com>
2014-07-08 11:18:02 -0400

    PHPUnit

--- readme.markdown
+++ readme.markdown
@@ -2,3 +2,25 @@ Requirements
 ====
 
 PHP 5.3.0 or greater
+
+Usage
+====
+
+Extend the following classes:
+
+* Service - Configure the following:
+  * Class name of your client
+  * Class name of your server
+  * Interface definition
+  * Class name of transport to use
+  * Class name of encoder to use
+* Server
+* Client
+
+Call `MyService::server()` to get an instance of your server.
+Call `MyService::client()` to get an instance of your client.
+
+TODO
+====
+
+* Set up for installation via composer
--- tests/clienttest.php
+++ tests/clienttest.php
@@ -3,23 +3,55 @@ error_reporting(E_ALL);
 
 include('setup.php');
 
-// Service knows where the endpoint is, which calls are available, which protocol, and which transport
-$client = MyService::client();
+class ClientTest extends \PHPUnit_Framework_TestCase {
+    protected $client;
 
-try {
-    $out = $client->reverse('hey', 5);
-    // Our custome MyClient class stores profiling data in the response, and makes the response publicly available
-    var_dump($client->response->oob('profile'));
-    var_dump($out);
-} catch (\Exception $e) {
-    echo implode("\r\n", $e->errors) . "\r\n";
-}
+    public function setUp() {
+        // Service knows where the endpoint is, which calls are available, which protocol, and which transport
+        $this->client = MyService::client();
+    }
 
-try {
-    $out = $client->badReturn();
-    var_dump($out);
-} catch (\Exception $e) {
-    echo implode("\r\n", $e->errors) . "\r\n";
+    public function test_simple_with_profiling() {
+        try {
+            $response = $this->client->reverse('hey', 5);
+            $this->assertEquals('yeh', $response);
+
+            // Our custome MyClient class stores profiling data in the response, and makes the response publicly available
+            $this->assertInternalType('array', $this->client->response->oob('profile'));
+            // Need to test profile format too
+
+        } catch (\Exception $e) {
+            echo 'One: ' . implode("\r\n", $e->errors) . "\r\n";
+        }
+    }
+
+    public function test_definition_enforcement() {
+
+        // Validate param type
+        try {
+            // Client calls with bad params throw exceptions here
+            $response = $this->client->reverse(false, 'should not be string');
+        } catch (\Exception $e) {
+            $this->assertObjectHasAttribute('errors', $e);
+            // Make sure there are 2 errors about param types
+            $this->assertCount(2, $e->errors);
+            // TODO - Make sure the errors are what we'd expect
+        }
+
+        // Validate return type
+        try {
+            // Bad return values on server side turn into error responses
+            $response = $this->client->badReturn();
+            $this->assertObjetHasAttribute('errors', $response);
+            // Make sure there's 1 error about return type
+            $this->assertCount(1, $response->errors);
+        } catch (\Exception $e) {
+            // Wait a minute, the client promotes response errors to exceptions? is that right?
+            // That means we never get the response object directly ... hmm, decisions
+            // TODO - should we throw exceptions, or return response objects with error messages?
+            echo implode("\r\n", $e->errors) . "\r\n";
+        }
+    }
 }
 
 
