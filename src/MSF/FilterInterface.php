6e81bee6d09063a3b67995e90a689c83c5284385
Alan W Szlosek Jr <alan.szlosek@gmail.com>
2014-07-23 20:06:37 -0400

    update comments

--- src/MSF/FilterInterface.php
+++ src/MSF/FilterInterface.php
@@ -2,8 +2,8 @@
 namespace MSF;
 
 // Each filter sees the request coming in, and the response going out (in reverse order through the filter stack)
-// BUT FILTERS SHOULDN'T MODIFY THE REQUEST OR RESPONSE BODY
 interface FilterInterface {
+    // Inside request(), annotate $request->response with error messages to signal an error
     public function request(\MSF\Request $request);
     public function response(\MSF\Response $response);
 }
